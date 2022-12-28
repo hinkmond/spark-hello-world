06f6318c699914b70c19a2a7535d4fd945064f12
Hinkmond Wong <hinkmond_wong@apple.com>
2022-12-27 21:26:03 -0800

    Fixed how log4j2 log level gets set (both in /opt/spark/conf/log4j2.xml and in the local JAR src/main/resources/log4j2.xml).  And, kept the SparkContext log level set by a Java property: log4j.debugMode.

--- src/main/java/com/hinkmond/SparkHelloWorld.java
+++ src/main/java/com/hinkmond/SparkHelloWorld.java
@@ -1,10 +1,8 @@
 package com.hinkmond;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.rdd.RDD;
-// DataFrame
+// AKA: DataFrame
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -17,33 +15,42 @@ import java.util.function.Predicate;
 
 
 public class SparkHelloWorld {
-    public static void main(String[] args) {
-        Optional<String> debugMode = Optional.ofNullable(System.getProperty("log4j.debugMode"))
-                                             .filter(Predicate.not(String::isEmpty));
+    static SparkSession spark;
 
-        debugMode.ifPresent(s -> Logger.getRootLogger()
-                                       .setLevel(Level.toLevel(s)));
-
-        SparkConf sparkConf = new SparkConf()
-                .setAppName("SparkHelloWorld")
-                .setMaster("local[*]");
-        SparkSession spark = SparkSession.builder()
+    static {
+        SparkConf sparkConf = new SparkConf().setAppName("SparkHelloWorld")
+                                             .setMaster("local[*]");
+        spark = SparkSession.builder()
                             .config(sparkConf)
                             .getOrCreate();
 
-        RDD<String> textFile = spark.sparkContext().textFile("/opt/spark/README.md",
-                1);
+        Optional<String> debugMode = Optional.ofNullable(System.getProperty("log4j.debugMode"))
+                                             .filter(Predicate.not(String::isEmpty));
+
+        debugMode.ifPresent(s -> {
+            System.out.println(">>> Setting log4j SparkContext log level to: " + s);
+            spark.sparkContext()
+                 .setLogLevel(s);
+        });
+    }
+
+    public static void main(String[] args) {
+        RDD<String> textFile = spark.sparkContext()
+                                    .textFile("/opt/spark/README.md", 1);
         System.out.println(textFile.count());
 
         List<String> data = Arrays.asList(("Java, 5000"), ("Scala, 3000"), ("Python, 100000"));
 
         // DataFrame
-        Dataset<Row> exampleDF = spark.sqlContext().createDataset(data, Encoders.STRING()).toDF();
+        Dataset<Row> exampleDF = spark.sqlContext()
+                                      .createDataset(data, Encoders.STRING())
+                                      .toDF();
         exampleDF.printSchema();
         exampleDF.show();
 
         // Convert
-        Dataset<Row> exampleTableDF = exampleDF.selectExpr("split(value, ',')[0] as PROG_LANG",
+        Dataset<Row> exampleTableDF =
+                exampleDF.selectExpr("split(value, ',')[0] as PROG_LANG",
                         "split(value, ',')[1] as NUM_DEV");
         exampleTableDF.printSchema();
         exampleTableDF.show();
